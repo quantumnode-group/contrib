3810afa0c3916318a4aa768b5ee1659fbdef72cc
Bryte Morio <brytemorio@qngnode.cc>
2020-07-01 00:28:38 +0100

    Typo correction

--- mine-v1.sh
+++ mine-v1.sh
@@ -11,13 +11,13 @@ passwd=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libboost-all-dev libboost-program-options-dev
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libboost-all-dev libboost-program-options-dev
 
-sudo apt-get install libminiupnpc-dev libzmq3-dev libprotobuf-dev protobuf-compiler unzip software-properties-common
+sudo apt-get install -y libminiupnpc-dev libzmq3-dev libprotobuf-dev protobuf-compiler unzip software-properties-common
 
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 }
 
 
